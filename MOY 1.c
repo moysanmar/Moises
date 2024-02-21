1bc2b797512cdde5f4727319a6d73bd3820eaef0
moysanmar <160264290+moysanmar@users.noreply.github.com>
2024-02-21 08:20:16 -0600

    suma de numeros del 1 al 100

---
+++ MOY 1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main()
+{
+
+int i = 1;
+int suma = 0;
+
+while(i < 101){
+
+       suma=suma+ i;
+  i++;
+
+}
+{
+
+
+
+  printf("%d\n",suma);
+}
+printf("La suma del 1 al 100 es: %d", suma);
+printf("\n\n");
+}
